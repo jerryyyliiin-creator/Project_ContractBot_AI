248e75083549b3473669ccaffab2d31c984dcd93
Jerry Lin <jerryyyliiin@gmail.com>
2025-11-20 22:43:29 -0800

    Comment out unused imports in 1_Analysis.py
    Comment out unused imports related to ContextualCompressionRetriever and LLMChainExtractor.

--- pages/1_Analysis.py
+++ pages/1_Analysis.py
@@ -12,8 +12,7 @@ from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_community.vectorstores import FAISS
 # from langchain.retrievers import ContextualCompressionRetriever
-from langchain_community.retrievers import ContextualCompressionRetriever
-from langchain.retrievers.document_compressors import LLMChainExtractor
+# from langchain.retrievers.document_compressors import LLMChainExtractor
 
 # Import Utility Libraries for S3 Storage and Pinecone Learning
 import storage_utils as storage
