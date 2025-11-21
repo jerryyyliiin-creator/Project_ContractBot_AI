c18ad1a6a7b857f490f199414b6b6cdd56b818fb
Jerry Lin <jerryyyliiin@gmail.com>
2025-11-20 22:59:59 -0800

    Update import for EnsembleRetriever module

--- pages/3_ContractBot.py
+++ pages/3_ContractBot.py
@@ -14,7 +14,8 @@ from pinecone import Pinecone
 
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain.retrievers import EnsembleRetriever
+# from langchain.retrievers import EnsembleRetriever
+from langchain_community.retrievers.ensemble import EnsembleRetriever
 from langchain.schema import HumanMessage, AIMessage, SystemMessage
 
 # Page Setting
